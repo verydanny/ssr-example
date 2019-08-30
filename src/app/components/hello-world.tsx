ea850d407c9aeb58a9b4e7f03183088fa93c56f8
Daniil Veremchuk <daniel@verydanny.com>
2019-08-30 01:38:41 -0400

    async module example

--- src/app/components/hello-world.tsx
+++ src/app/components/hello-world.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
 
+import('rambdax').then((R: typeof import('rambdax')) => {
+  const addTen = (x: number) => x + 10
+  const subtractOne = (x: number) => x - 1
+  console.log(
+    R.compose(
+      addTen,
+      subtractOne
+    )(20)
+  )
+})
+
 export const HelloWorld = () => (
   <div> Hello World, fams. Breh everything is working</div>
 )
