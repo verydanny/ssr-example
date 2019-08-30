bb79086302cafc0ec42e24ed277e833d7a6fb803
Daniil Veremchuk <daniel@verydanny.com>
2019-08-29 22:57:47 -0400

    even loads async

--- src/server/middleware/logger.ts
+++ src/server/middleware/logger.ts
@@ -1,6 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Request, Response, NextFunction } from 'express'
 
+import('./render').then(module => {
+  console.log('Even loading async things server side from memory works')
+  console.log('AsyncModule ', module)
+})
+
 export const logger = (_req: Request, _res: Response, next: NextFunction) => {
   console.log('Middleware, You bingor maximus')
   next()
